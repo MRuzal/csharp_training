1b578c0edfd7e5326d9ad9a81e6db32b21d6421f
Ruzal Mukhamadiev <mr_mukhamadiev@mail.ru>
2021-02-15 19:55:09 +0300

    Тест создания контакта

---
+++ Addressbook-web-test/Addressbook-web-test/ContactCreation/ContactData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string firstname = "";
+        private string lastname = "";
+
+
+        public string Firstname
+        {
+
+            get
+            {
+                return firstname;
+            }
+
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+
+            set
+            {
+                lastname = value;
+            }
+        }
+    }
+}
+
